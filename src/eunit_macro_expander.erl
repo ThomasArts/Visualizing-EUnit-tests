353f4b0e940b4ec7fba0a58c2089c528a023286d
Simon Thompson <s.j.thompson@kent.ac.uk>
2011-03-03 15:02:25 +0100

    Added files for tracing the operation of EUnit and macro annotation to record test scopes and positive / negative modalities.

---
+++ src/eunit_macro_expander.erl
@@ -0,0 +1,139 @@
+-module(eunit_macro_expander).
+
+-compile(export_all).
+
+%% File is a String, "Module.erl".
+
+file(File) ->
+    {ok, Trees} = epp_dodger:parse_file(File),
+    Macros1 = [makeMacroTree(MName,test_negative) || MName <- [assertError,
+						             assertExit,
+						             assertException,
+						             assertThrow]],
+    Macros2 = [makeMacroTree(MName,negative_wrap) || MName <- ['_assertError',
+						             '_assertExit',
+						             '_assertException',
+						             '_assertThrow']],
+    Tree = erl_syntax:form_list(Trees),
+    NewTree = erl_syntax_lib:map(fun transformer/1,Tree),
+    {Head,Body} = split(erl_syntax:form_list_elements(NewTree)),
+    MacroTree = erl_syntax:form_list(Head++Macros1++Macros2++Body),
+    Strings = erl_prettypr:format(MacroTree),
+    file:write_file("/tmp/"++filename:basename(File),Strings).
+
+%% Transforms a tree, doing two things
+%%  - replaces macros of the form assertXXX
+%%    by assertXXXTrace, when these are negative
+%%    e.g. assertError,
+%%  - wraps the bodies of tests and test objects inside
+%%    calls to test_wrap and test__wrap (from tracing).
+
+transformer(Tree) ->
+    case erl_syntax:type(Tree) of
+	macro ->
+	    MName = erl_syntax:macro_name(Tree),
+	    MacroAtomName =
+		case  erl_syntax:type(MName) of
+		    atom -> erl_syntax:atom_value(MName);
+		    variable -> erl_syntax:variable_name(MName)
+		end,
+            case MacroAtomName of
+		assertError ->
+		   erl_syntax:macro(erl_syntax:atom(assertErrorTrace),
+				     erl_syntax:macro_arguments(Tree));
+		'_assertError' ->
+		   erl_syntax:macro(erl_syntax:atom('_assertErrorTrace'),
+				     erl_syntax:macro_arguments(Tree));
+		assertExit ->
+		   erl_syntax:macro(erl_syntax:atom(assertExitTrace),
+				     erl_syntax:macro_arguments(Tree));
+		'_assertExit' ->
+		   erl_syntax:macro(erl_syntax:atom('_assertExitTrace'),
+				     erl_syntax:macro_arguments(Tree));
+		assertException ->
+		   erl_syntax:macro(erl_syntax:atom(assertExceptionTrace),
+				     erl_syntax:macro_arguments(Tree));
+		'_assertException' ->
+		   erl_syntax:macro(erl_syntax:atom('_assertExceptionTrace'),
+				     erl_syntax:macro_arguments(Tree));
+		assertThrow ->
+		   erl_syntax:macro(erl_syntax:atom(assertThrowTrace),
+				     erl_syntax:macro_arguments(Tree));
+		'_assertThrow' ->
+		   erl_syntax:macro(erl_syntax:atom('_assertThrowTrace'),
+				     erl_syntax:macro_arguments(Tree));
+		_ -> Tree
+	    end;
+	function ->
+	    FName = erl_syntax:function_name(Tree),
+	    case erl_syntax:type(FName) of
+	 	atom ->
+	 	    case is_testFun(erl_syntax:atom_value(FName)) of
+	 		test ->
+	 		    wrap_test_function(Tree,FName,tracing,test_wrap);
+	 		test_object ->
+	 		    wrap_test__function(Tree,FName,tracing,test__wrap);
+	 		_ -> Tree
+	 	    end;
+	 	_ -> Tree
+	     end;
+	_ -> Tree
+    end.
+
+%% Builds the macro definition
+%%  -define(NewName(X,Y),?OldName(X,Y)).
+
+makeMacroTree(OldName) ->
+    makeMacroTree(OldName,test_negative).
+
+makeMacroTree(OldName,Wrapper) ->
+    NewName = list_to_atom(lists:concat([OldName,"Trace"])),
+    X = erl_syntax:variable("X"),
+    Y = erl_syntax:variable("Y"),
+    LHS = erl_syntax:application(erl_syntax:atom(NewName),[X,Y]),
+    ModName = erl_syntax:atom(tracing),
+    FunName = erl_syntax:atom(Wrapper),
+    MacApp = erl_syntax:macro(erl_syntax:atom(OldName),[X,Y]),
+    RHS = erl_syntax:application(erl_syntax:module_qualifier(ModName,FunName),[MacApp]),
+    erl_syntax:attribute(erl_syntax:atom(define),[LHS,RHS]).
+
+%% Splits the form list after the attributes
+%% (and so before the functions).
+
+split(Fs) ->
+    lists:splitwith(fun (F) ->
+			     erl_syntax:type(F) == attribute end,
+		    Fs).
+
+%% Is an atom a test: ..._test,
+%% a test object: ..._test_,
+%% or not?
+
+is_testFun(Atom) ->    
+    String = lists:reverse(atom_to_list(Atom)),
+    case String of
+	"_tset_"++ _ ->
+	    test_object;
+	"tset_"++ _ ->
+	    test;
+	_ ->
+	   no_test
+    end.
+
+wrap_test_function(Tree,FName,Module,Function) ->
+    [Clause] = erl_syntax:function_clauses(Tree),
+    Body = erl_syntax:clause_body(Clause),
+    Fun = erl_syntax:fun_expr([erl_syntax:clause([],none,Body)]),
+    ModName = erl_syntax:atom(Module),
+    FunName = erl_syntax:atom(Function),
+    RHS = erl_syntax:application(erl_syntax:module_qualifier(ModName,FunName),[Fun]),
+    erl_syntax:function(FName,[erl_syntax:clause([],none,[RHS])]).
+
+wrap_test__function(Tree,FName,Module,Function) ->
+    [Clause] = erl_syntax:function_clauses(Tree),
+    [Body] = erl_syntax:clause_body(Clause),
+    ModName = erl_syntax:atom(Module),
+    FunName = erl_syntax:atom(Function),
+    RHS = erl_syntax:application(erl_syntax:module_qualifier(ModName,FunName),[Body]),
+    erl_syntax:function(FName,[erl_syntax:clause([],none,[RHS])]).
+
---
+++ src/eunit_tracing.erl
@@ -0,0 +1,141 @@
+-module(eunit_tracing).
+
+-export([t/0,t/1]).
+
+-export([test_start/0, test_end/0, test_group_start/0,
+         test_group_end/0, test_negative/0, test_negative/1]).
+
+-export([map_tuple/2, test_wrap/1, test__wrap/1,
+         test__group_wrap/1, negative_wrap/1]).
+
+%%
+%% Top-level function to initiate tracing.
+%%
+
+%% Tracing function which traces calls to functions in frequency
+%% as well as "skip" functions here used to punctuate the trace.
+
+t() ->
+    t(frequency).
+
+t(Mod)
+  when is_atom(Mod) ->
+    code:load_file(Mod),
+    code:load_file(addTestSuffix(Mod)),
+    erlang:trace(all, true, [call]),
+    erlang:trace_pattern({tracing, test_start, '_'}, true, [local]),
+    erlang:trace_pattern({tracing, test_end, '_'}, true, [local]),    
+    erlang:trace_pattern({tracing, test_group_start, '_'}, true, [local]),
+    erlang:trace_pattern({tracing, test_group_end, '_'}, true, [local]),    
+    erlang:trace_pattern({tracing, test_negative, '_'}, true, [local]),    
+    erlang:trace_pattern({Mod, '_', '_'}, true, [global]).
+
+addTestSuffix(Mod) ->
+    list_to_atom(lists:concat([Mod,"_test"])).
+
+%%
+%% Functions to punctuate the trace.
+%%
+
+%% These are "skip" functions with no effect other than 
+%% to be called and so to appear in the trace.
+
+test_start() ->
+    ok.
+
+test_end() ->
+    ok.
+
+test_group_start() ->
+    ok.
+
+test_group_end() ->
+    ok.
+
+test_negative() ->
+    ok.
+
+test_negative(Test) ->
+    Test.
+
+%%
+%% Wrapping up tests
+%%
+
+%% Map over a tuple (done by conversion to/from a list).
+
+map_tuple(F,T) ->
+    list_to_tuple(lists:map(F,tuple_to_list(T))).
+
+%% Wrap a ..._test()
+%% Can be positive or negative.
+
+test_wrap(F) ->
+    test_start(),
+    F(),
+    test_end().
+
+%% Wrap a single component of a ..._test_()
+%% Can be positive or negative.
+
+test__wrap(F)
+  when is_function(F) ->
+    fun () ->
+	    test_start(),
+	    F(),
+	    test_end()
+    end;  
+test__wrap(F)
+  when is_tuple(F) ->
+    case F of
+	{setup,Setup,Teardown,Tests} ->
+	    {setup,
+	     % fun () ->
+	     % 	     test_group_start(),
+	     % 	     Setup()
+	     % end,
+	     Setup,
+	     % fun (R) ->
+	     % 	     Teardown(R),
+	     % 	     test_group_end()
+	     % end,
+	     Teardown,
+	     test__wrap(Tests)};
+	_ ->    
+	    map_tuple(fun test__wrap/1,F)
+    end;
+test__wrap(F)
+  when is_list(F)->
+    {setup,
+     fun () -> test_group_start() end,
+     fun (_) -> test_group_end() end,
+     lists:map(fun test__wrap/1,F)};
+test__wrap(F) ->
+    F.
+
+%% Wrap a group (list) of tests.
+%% Uses a fixture to do the wrapping.
+
+%% 2/3/11 No longer used: folded into test__wrap/1
+
+test__group_wrap(Tests) ->
+	    [{setup,
+	      fun () -> test_group_start() end,
+	      fun (_) -> test_group_end() end,
+	      lists:map(fun test__wrap/1,Tests)}
+	    ].  
+
+%% Mark a test as negative.
+%% Used in the redefinition of _assertError etc.
+
+negative_wrap(F)
+  when is_function(F) ->
+    fun () ->
+	    F(),
+	    test_negative()
+    end;  
+negative_wrap(F)
+  when is_tuple(F) -> 
+    map_tuple(fun negative_wrap/1,F);
+negative_wrap(F) ->
+    F.
