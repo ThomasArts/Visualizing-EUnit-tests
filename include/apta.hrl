ff6479a3595af9895413b93ea6c5932662ba7c5e
Thomas Arts <thomas.arts@quviq.com>
2011-06-01 09:33:03 +0200

    Replaced the APTA computation by, in my eyes, simpler piece of Erlang code.

--- include/apta.hrl
+++ include/apta.hrl
@@ -1,3 +1,3 @@
 %% Automaton generation data
-%% lastState, foundAlphabet, foundTransitions, rejectionStates
--record(agd, {lastSt = 0, alph = [], tr = [], rSt = []}).
+%% lastState, foundAlphabet, foundTransitions, rejectionStates, acceptingStates
+-record(agd, {lastSt = 0, alph = [], tr = [], rSt = [], aSt = []}).
