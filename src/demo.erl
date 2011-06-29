59229b72bed40723fc9c6c5e732819bd664ada8d
Thomas Arts <thomas.arts@quviq.com>
2011-06-29 12:17:45 +0200

    Modified demo to be even cooler!

--- src/demo.erl
+++ src/demo.erl
@@ -9,13 +9,17 @@
 
 frequency() ->
   % first run the tests to get the traces
-  {Pos,Neg} = eunit_to_fsm:dynamic("../examples/frequency_tests.erl",
+  {Pos,Neg} = eunit_to_fsm:dynamic("../examples/frequency.erl",
                                    [],fun({M,F,A}) when {M,F}=/={frequency,init} -> {M,F,A} end),
+
   % second create an automata
   Automata = bluefringe:qsm({Pos,Neg}),
+
   % third transform automata into QuickCheck template
   String = bluefringe_fsm:eqc_fsm(Automata,frequency),
   ok = file:write_file("frequency_eqc.erl",String),
+
   % Compile the QuickCheck code
   code:purge(frequency_eqc),
-  compile:file("frequency_eqc.erl").
+  compile:file("frequency_eqc.erl"),
+  code:load_file(frequency_eqc).
