5164b6382707298393e96024f2a5d7249e3fb582
Pablo Lamela Seijas <pablo.lamela@quviq.de>
2011-08-03 16:10:13 +0200

    Adapted DOT to show Extra Info as a edgehint

--- src/bluefringe_dot.erl
+++ src/bluefringe_dot.erl
@@ -9,13 +9,21 @@
 -module(bluefringe_dot).
 
 %% API
--export([visualize/2, visualize_debug/2]).
+-export([visualize/1, visualize_debug/1]).
 
-visualize(Automata,Abstract) -> visualize(Automata,Abstract,"jpeg").
+-import(bluefringe_apta, [ksort/2]).
 
-visualize_debug(Automata,Abstract) -> visualize(Automata, Abstract,"svg").
+simplePP(X) -> io_lib:format("~p", [X]).
+simplePP2(X) -> io_lib:format("~p", [X]).
 
-visualize({fa,States,_Tokens,InitState,Transitions,FailingStates},Abstract,Type) ->
+visualize(Automata) -> visualize(Automata, fun simplePP/1, fun simplePP2/1).
+visualize_debug(Automata) -> visualize_debug(Automata, fun simplePP/1, fun simplePP2/1).
+
+visualize(Automata,PPAbstracted,PPNonAbstracted) -> visualize(Automata,PPAbstracted,PPNonAbstracted,"jpeg").
+
+visualize_debug(Automata,PPAbstracted,PPNonAbstracted) -> visualize(Automata,PPAbstracted,PPNonAbstracted,"svg").
+
+visualize({fa,States,_Tokens,InitState,Transitions,FailingStates},PPAbstracted,PPNonAbstracted,Type) ->
     Name = "eunit_fsm",
     Dot = lists:flatten(["digraph G {\n",
 			 translate_initstate(InitState),
@@ -23,7 +31,7 @@ visualize({fa,States,_Tokens,InitState,Transitions,FailingStates},Abstract,Type)
 			 lists:map(fun translate_normalstate/1,
 				   (States -- [InitState]) -- FailingStates),
 			 lists:map(fun translate_transition/1,
-				   mix_transitions(Transitions,Abstract)),
+				   mix_transitions(Transitions,PPAbstracted,PPNonAbstracted)),
 			 "}\n"]),
     file:write_file(Name++".dot", list_to_binary(Dot)),
     oscmd("GRAPHVIZ","dot","-T"++Type++" "++Name++".dot -o"++Name++"."++Type),
@@ -45,15 +53,16 @@ oscmd(EnvVar,Default,Args) ->
 removeLast([_|[]]) -> [];
 removeLast([H|T]) -> [H|removeLast(T)].
 
+flatten_and_remove_last(List) -> {Abstracted, NoAbstracted} = lists:unzip(List),
+								 {removeLast(lists:flatten(Abstracted)), removeLast(lists:flatten(NoAbstracted))}.
 
-mix_transitions(Transitions,Abstract) ->
-    lists:map(fun ({A,B}) ->
-		      {A, removeLast(lists:flatten(lists:usort(
-                                       [io_lib:format("~p~n", [Abstract(C)]) || {D, C, E} <- Transitions, D =:= A, E =:= B]
-				      ))),
-                       B}
-	      end,
-	      lists:usort([{A, B} || {A,_,B} <- Transitions])).
+mix_transitions(Transitions,PPAbstracted,PPNonAbstracted) ->
+	lists:map(fun ({A,B}) ->
+					   {A, flatten_and_remove_last([{lists:flatten(io_lib:format("~s~n", [PPAbstracted(C)])),
+							 lists:flatten(io_lib:format("~s: ~s~n", [PPAbstracted(C), PPNonAbstracted(EI)]))}
+						 || {D, {C, EI}, E} <- Transitions, D =:= A, E =:= B]), B}
+			  end,
+			  lists:usort([{A, B} || {A,_,B} <- Transitions])).
 
 translate_initstate(InitState) ->
     lists:flatten(io_lib:format("  ~p [shape=doublecircle];~n", [InitState])).
@@ -61,6 +70,6 @@ translate_normalstate(NormalState) ->
     lists:flatten(io_lib:format("  ~p [shape=circle];~n", [NormalState])).
 translate_failingstate(FailingState) ->
     lists:flatten(io_lib:format("  ~p [shape=polygon,sides=8,peripheries=3,color=red,style=filled]~n", [FailingState])).
-translate_transition({Ori, Label, Dest}) ->
-    lists:flatten(io_lib:format("  ~p -> ~p [label=~p];~n",
-		  [Ori, Dest, Label])).
+translate_transition({Ori, {Label, History}, Dest}) ->
+    lists:flatten(io_lib:format("  ~p -> ~p [label=~p, edgetooltip=~p];~n",
+		  [Ori, Dest, Label, History])).
